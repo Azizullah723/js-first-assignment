4c21b008f62a2f684cad267816a3533b501e6399
Azizullah723 <155417984+Azizullah723@users.noreply.github.com>
2024-03-06 08:53:52 +0430

    Add files via upload

---
+++ first-js-assignment.js
@@ -0,0 +1,44 @@
+const country="Afghanistan";
+
+const continent="Asia";
+
+let population=30;
+
+console.log("which country dou you live? " +" i live in ",country )
+console.log("which continent you country is?  ",continent )
+console.log("how many peoples are living in your county?  " ,population, "millions")
+const ismarried=true;
+const isunmarried=false;
+let languages;
+console.log(ismarried +"  married");
+console.log(isunmarried+"  unmarried");
+console.log("data type of country  "+ typeof country);
+console.log("data type of population  "+ typeof population);
+console.log("data type of lang  "+ typeof languages);
+languages="dari";
+console.log("lang: "+languages);
+let halfpopulation=population/2;
+console.log("half of population = " + halfpopulation +" millions" )
+let incresedpop=population+1;
+console.log("incresed by 1 population: "+ incresedpop +"millions")
+let finland=6;
+if(population>finland){
+    console.log(" we have more population then finland")
+}
+let avgpop=33;
+if(population>=avgpop){
+    console.log("we are more than avg population")
+}
+else{
+    console.log("we are less than avg pop")
+}
+
+let x=true;
+let y=false;
+console.log(x && y);
+console.log(x || y);
+console.log(!x)
+
+
+//second assignment
+console.log(prompt("how many n"))
